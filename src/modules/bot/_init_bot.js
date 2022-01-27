97708261909e69649809dce446a861df0de35620
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-01-27 08:20:00 +0100

    ft-Welcome bot

--- src/modules/bot/_init_bot.js
+++ src/modules/bot/_init_bot.js
@@ -1,23 +1,23 @@
-// const { App } = require('@slack/bolt');
-// const { KEYS } = require("../../constants/keys")
+const { App } = require('@slack/bolt');
+const { KEYS } = require("../../constants/keys")
 
-// exports.init_bot = async() => {
-//     try {
-//         const app = new App({
-//             signingSecret: KEYS.SIGNING_SECRET,
-//             token: KEYS.OAUTH_TOKEN,
-//           });
+exports.init_bot = async() => {
+    try {
+        const app = new App({
+            signingSecret: KEYS.SIGNING_SECRET,
+            token: KEYS.OAUTH_TOKEN,
+          });
           
-//           /* Add functionality here */
+          /* Add functionality here */
           
-//           (async () => {
-//             // Start the app
-//             await app.start(KEYS.PORT || 3000);
+          (async () => {
+            // Start the app
+            await app.start(KEYS.PORT || 3000);
           
-//             console.log('⚡️ Bolt app is running!');
-//             return app;
-//           })();
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
+            console.log('⚡️ Bolt app is running!');
+            return app;
+          })();
+    } catch (error) {
+        console.error(error);
+    }
+}
