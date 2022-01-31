23ab6a0cb316478005cc1895caf2b346339ee8de
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-01-31 08:37:23 +0100

    chore-Interactive Feature

--- src/modules/bot/events/message.listener.js
+++ src/modules/bot/events/message.listener.js
@@ -3,14 +3,14 @@ const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
 const slackEvents = createEventAdapter(slackSigningSecret)
 
 exports.listenForEvents = (app) => {
-  app.use('/', slackEvents.requestListener())
+  app.use('/events', slackEvents.requestListener())
 }
 
 slackEvents.on('message', (event) => {
   console.log(`Received an app_mention event from user ${event.user} in channel ${event.channel}`)
 })
 
-// All errors in listeners are caught here. If this weren't caught, the program would terminate.
+// All errors in listeners are caught here.
 slackEvents.on('error', (error) => {
   console.log(`error: ${error}`)
 })
