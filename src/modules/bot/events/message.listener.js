2e616c4cf61608b086656c0e4eae58f58d9aec72
Adjaro Ogaga <ogagaadjaro@gmail.com>
2022-01-31 22:00:57 +0100

    chore-FIxed random user

--- src/modules/bot/events/message.listener.js
+++ src/modules/bot/events/message.listener.js
@@ -1,18 +1,29 @@
 const { Router } = require("express");
 const router = Router();
-const { createEventAdapter } = require('@slack/events-api')
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
-const slackEvents = createEventAdapter(slackSigningSecret)
+const KEYS = require("../../../constants/keys");
+const { createEventAdapter } = require("@slack/events-api");
+const slackSigningSecret = KEYS.KEYS.SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
+const port = 3000;
+
+(async () => {
+  // Start the built-in server
+  const server = await slackEvents.start(port);
+
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server.address().port}`);
+  // All errors in listeners are caught here.
+  slackEvents.on("error", (error) => {
+    console.log(`error: ${error}`);
+  });
+})();
 
 exports.listenForEvents = () => {
-    slackEvents.requestListener();
-}
+  slackEvents.requestListener();
+};
 
-slackEvents.on('message', (event) => {
-  console.log(`Received an app_mention event from user ${event.user} in channel ${event.channel}`)
-})
-
-// All errors in listeners are caught here.
-slackEvents.on('error', (error) => {
-  console.log(`error: ${error}`)
-})
+slackEvents.on("message", (event) => {
+  console.log(
+    `Received a message event from user ${event.user} in channel ${event.channel}`
+  );
+});
